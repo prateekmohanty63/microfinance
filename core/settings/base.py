ea96b98ea6f38aa31a406d3ac03bd31a642ede11
Matt Capelli <matt@simpledatatools.com>
2022-02-25 07:35:25 +0300

    Remove KYC app, integrate with accounts

--- core/settings/base.py
+++ core/settings/base.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'organizations',
     'onboarding',
     'loans',
-    'kyc',
 
 ]
 
--- kyc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KycConfig(AppConfig):
-    name = 'kyc'
